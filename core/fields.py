5bd265c5ffbf392b7fd77ba08540e24918c6eba2
Loïc Pinsard <muream@gmail.com>
2018-12-11 21:47:43 +0100

    feat: streamline the maya attribute creation and setter

--- core/fields.py
+++ core/fields.py
@@ -1,28 +1,37 @@
 import maya.cmds as cmds
 
+
 class Attribute(object):
     def __init__(self, instance, field):
-        self.node = instance
-        self.attr_name = '.'.join([self.node.name, field.name])
-        if not cmds.attributeQuery(field.name, node=self.node.name, exists=True):
+        self.field = field
+        self.attr_name = '.'.join([instance.name, field.name])
+        if not cmds.attributeQuery(field.name, node=instance.name, exists=True):
             cmds.addAttr(
-                self.instance.name,
+                instance.name,
                 longName=field.name,
-                attributeType=field.attr_type,
+                **field.create_attr_args
             )
 
     def set(self, value):
-        cmds.setAttr(self.attr_name, value)
+        casted_value = self.field.cast(value)
+        cmds.setAttr(self.attr_name, casted_value, **self.field.set_attr_args)
 
     def get(self):
         return cmds.getAttr(self.attr_name)
 
+
 class Field(object):
+    create_attr_args = {}
+    set_attr_args = {}
+
     def __init__(
         self,
         name=None,
+        as_list=False,
     ):
         self.name = name
+        if as_list:
+            self.create_attr_args['multi'] = True
         self._attrs = {}
 
     def __get__(self, instance, instancetype=None):
@@ -37,5 +46,44 @@ class Field(object):
         return Attribute(instance, self)
 
 class IntField(Field):
-    attr_type = 'long'
+    create_attr_args = {
+        'attributeType': 'long'
+    }
 
+    def cast(self, value):
+        return int(value)
+
+class FloatField(Field):
+    create_attr_args = {
+        'attributeType': 'double'
+    }
+
+    def cast(self, value):
+        return float(value)
+
+class BoolField(Field):
+    create_attr_args = {
+        'attributeType': 'bool'
+    }
+
+    def cast(self, value):
+        return bool(value)
+
+class StringField(Field):
+    create_attr_args = {
+        'dataType': 'string'
+    }
+    set_attr_args = {
+        'type': 'string'
+    }
+
+    def cast(self, value):
+        return str(value)
+
+class ObjectField(StringField):
+    def cast(self, value):
+        value = super(ObjectField, self).cast(value)
+        if cmds.objExists(value):
+            return value
+        else:
+            raise ValueError('node `{}` does not exist'.format(value))
