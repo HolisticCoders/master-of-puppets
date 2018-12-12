3145a8c6c96327630923c59dddfe02dc2084218e
Loïc Pinsard <muream@gmail.com>
2018-12-12 20:58:50 +0100

    WIP: keep track of the added rig modules WIP: manage the better management of the deformation skeleton

--- core/rig.py
+++ core/rig.py
@@ -6,17 +6,16 @@ import maya.cmds as cmds
 from icarus.core.icarusNode import IcarusNode
 from icarus.modules import all_rig_modules
 from icarus.config import default_modules
-from icarus.core.fields import (
-    ObjectField,
-)
+from icarus.core.fields import ObjectField
 
 
 class Rig(IcarusNode):
 
     modules_group = ObjectField('modules_group')
     extras_group = ObjectField('extras_group')
-    rig_modules = ObjectField('rig_modules', as_list=True)
-    skeleton = ObjectField('skeleton', as_list=True)
+    skeleton_group = ObjectField('skeleton_group')
+    rig_modules = ObjectField('rig_modules', multi=True)
+    skeleton = ObjectField('skeleton', multi=True)
 
     def __init__(self):
         super(Rig, self).__init__('RIG')
@@ -36,6 +35,12 @@ class Rig(IcarusNode):
                 name='EXTRAS',
             ))
             cmds.parent(self.extras_group.get(), 'RIG')
+        if not cmds.objExists('SKELETON'):
+            self.skeleton_group.set(cmds.createNode(
+                'transform',
+                name='SKELETON',
+            ))
+            cmds.parent(self.skeleton_group.get(), 'RIG')
 
     def _add_default_modules(self):
         for module_type, data in default_modules.iteritems():
@@ -46,12 +51,19 @@ class Rig(IcarusNode):
         if module_type not in all_rig_modules:
             raise Exception("Module Type {} is not valid".format(module_type))
 
+        # instantiate the new module from the pool of available modules.
         new_module = all_rig_modules[module_type](*args, **kwargs)
         cmds.parent(new_module.name, self.modules_group.get())
         new_module.initialize()
+
+        # add the module's deform joints to the rig's skeleton
         for joint in new_module.deform_joints.get():
             skel = self.skeleton.get()
             skel.append(joint)
             self.skeleton.set(skel)
-        self.rig_modules = self.rig_modules.get().append(new_module)
+
+        mods = self.rig_modules.get()
+        mods.append(new_module.name)
+        self.rig_modules.set(mods)
+
         return new_module
