c4ee3f3fd7efc5aa50f59c13e8743e04c1c0a934
Loïc Pinsard <muream@gmail.com>
2018-12-12 20:53:24 +0100

    Feat: added update and publish methods to the module

--- core/module.py
+++ core/module.py
@@ -29,10 +29,27 @@ class RigModule(IcarusNode):
         """
         raise NotImplementedError
 
+    def update(self):
+        """Update the placement based on the module's fields.
+
+        You need to overwrite this method in your subclasses.
+        """
+        raise NotImplementedError
+
     def build(self):
         """Actual rigging of the module.
 
         The end result should _always_ drive your module's deform joints
+        You need to overwrite this method in your subclasses.
+        """
+        raise NotImplementedError
+
+    def publish(self):
+        """Clean the rig for the animation.
+
+        Nothing in there should change how the rig works.
+        It's meant to hide some stuff that the rigger would need after the build
+        maybe lock some attributes, set some default values, etc.
         You need to overwrite this method in your subclasses.
         """
         raise NotImplementedError
