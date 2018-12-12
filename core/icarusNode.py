c7d7002590b512036174cff7d4f5ec2c866f12ae
Loïc Pinsard <muream@gmail.com>
2018-12-12 20:52:31 +0100

    clean up IcarusNode to be as bare bones as possible for now

--- core/icarusNode.py
+++ core/icarusNode.py
@@ -2,7 +2,7 @@ import maya.cmds as cmds
 
 
 class IcarusNode(object):
-    def __init__(self, name, node_type=None, parent=None):
+    def __init__(self, name, node_type=None):
         """Create the node if it doesn't already exist."""
         self.name = name
         if not cmds.objExists(name):
@@ -11,8 +11,5 @@ class IcarusNode(object):
             elif node_type not in cmds.allNodeTypes():
                 raise Exception("Specified `{}` node_type is not a valid maya node.".format(node_type))
                 return
-            if parent is not None and not cmds.objExists(parent):
-                raise Exception("Parent node `{}` does not exist.".format(parent))
-                return
-            cmds.createNode(node_type, name=name, parent=parent)
+            cmds.createNode(node_type, name=name)
 
