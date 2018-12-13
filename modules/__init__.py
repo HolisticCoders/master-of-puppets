5f1dcd3ca3d74a047a3df2e53f3d4e2201190180
Loïc Pinsard <muream@gmail.com>
2018-12-13 20:07:07 +0100

    remove prints

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,14 +5,10 @@ import importlib
 all_rig_modules = {}
 for mod in os.listdir(os.path.dirname(__file__)):
     if mod.endswith('.pyc'):
-        # print "skipping", mod
         continue
     if mod == os.path.basename(__file__):
-        # print "skipping", mod
         continue
     mod_name = 'icarus.modules.' + mod.split('.')[0]
     current_mod = importlib.import_module(mod_name)
-    # print "Current Mod", current_mod
-    # print dir(current_mod)
     for rig_module in current_mod.exported_rig_modules:
         all_rig_modules[rig_module.__name__] = rig_module
