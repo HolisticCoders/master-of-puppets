ca7fa53c5f4b7b6e7d8727f679bbcdc199d2d205
Loïc Pinsard <muream@gmail.com>
2018-12-26 23:24:31 +0100

    quickFix: rename chain_length to joint_count

--- modules/chain.py
+++ modules/chain.py
@@ -7,7 +7,7 @@ import icarus.dag
 
 class Chain(RigModule):
 
-    chain_length = IntField(
+    joint_count = IntField(
         defaultValue=1,
         hasMinValue=True,
         minValue=1
@@ -15,11 +15,11 @@ class Chain(RigModule):
 
     def initialize(self, *args, **kwargs):
         joints = []
-        for i in range(self.chain_length.get()):
+        for i in range(self.joint_count.get()):
             self._add_deform_joint()
 
     def update(self):
-        self._update_chain_length()
+        self._update_joint_count()
 
     def build(self):
         parent = self.controls_group.get()
@@ -46,12 +46,12 @@ class Chain(RigModule):
             parent = deform_joints[-1]
         return super(Chain, self)._add_deform_joint(parent=parent)
 
-    def _update_chain_length(self):
+    def _update_joint_count(self):
         deform_joints = self.deform_joints_list.get()
         if deform_joints is None:
             deform_joints = []
 
-        diff = self.chain_length.get() - len(deform_joints)
+        diff = self.joint_count.get() - len(deform_joints)
         if diff > 0:
             for index in range(diff):
                 new_joint = self._add_deform_joint()
