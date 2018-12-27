eab410124a1ad2daa16853eb6c064b86d2598928
Loïc Pinsard <muream@gmail.com>
2018-12-27 18:53:03 +0100

    post scripts: try to run the scripts only if the directory exists

--- postscript.py
+++ postscript.py
@@ -3,12 +3,14 @@ import os
 import maya.cmds as cmds
 
 
-def run_post_scripts(step):
+def run_scripts(step):
+    print "Running {} scripts".format(step)
     scene_path = os.path.dirname(cmds.file(query=True, sceneName=True))
     scripts_path = os.path.join(scene_path, 'SCRIPTS', step)
-    for script_name in os.listdir(scripts_path):
-        script_path = os.path.join(scripts_path, script_name)
-        mod = imp.load_source(script_name, script_path)
-        mod.run()
+    if os.path.isdir(scripts_path):
+        for script_name in os.listdir(scripts_path):
+            script_path = os.path.join(scripts_path, script_name)
+            mod = imp.load_source(script_name, script_path)
+            mod.run()
 
 
