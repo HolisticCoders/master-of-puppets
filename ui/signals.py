099364085cbe9793c92db2bef5a9d300b38e8a7e
Aphosis <alexy.long@orange.fr>
2019-01-11 00:02:55 +0100

    Added `icarus.ui.signals` documentation

--- ui/signals.py
+++ ui/signals.py
@@ -1,3 +1,4 @@
+"""Minimal observer/publisher implementation for all your GUI needs."""
 from collections import defaultdict
 from weakref import WeakKeyDictionary
 
@@ -5,10 +6,32 @@ _SIGNALS = defaultdict(list)
 
 
 def observe(name, func):
+    """Subscribe ``func`` to a publisher.
+
+    :param name: Signal to observe. When ``name`` is called
+                 by :func:`publish`, ``func`` will be called.
+    :param func: Function to register to the ``name`` signal.
+    :type name: str
+    :type func: callable
+    """
     _SIGNALS[name].append(func)
 
 
 def publish(name, *args, **kwargs):
+    """Emits a signal to all observers subscribed.
+
+    You can use ``args`` and ``kwargs`` to call subscribers
+    with these arguments.
+
+    Observers return values are collected and returned in a
+    :class:`weakref.WeakKeyDictionary` keyed by observer.
+
+    :param name: Name of the signal to emit.
+                 Observers subscribed to the same
+                 ``name`` will be notified.
+    :type name: str
+    :rtype: weakref.WeakKeyDictionary
+    """
     ret = WeakKeyDictionary()
     for func in _SIGNALS[name]:
         res = func(*args, **kwargs)
