4a854f0d526b2750f3114e314b43192c770ff47d
Loïc Pinsard <muream@gmail.com>
2019-01-14 21:16:30 +0100

    post script: pass the name of the file without extension to imp.load_source

--- postscript.py
+++ postscript.py
@@ -13,6 +13,8 @@ def run_scripts(step):
     scripts_path = os.path.join(scene_path, 'SCRIPTS', step)
     if os.path.isdir(scripts_path):
         for script_name in os.listdir(scripts_path):
+            module_name = os.path.splitext(script_name)[0]
             script_path = os.path.join(scripts_path, script_name)
-            mod = imp.load_source(script_name, script_path)
+            mod = imp.load_source(module_name, script_path)
+            logger.info("Running script: {}".format(module_name))
             mod.run()
