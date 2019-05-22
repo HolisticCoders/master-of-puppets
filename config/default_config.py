d9d03aeab786fa96f889b58ed66124a0d47d85e4
Loïc Pinsard <muream@gmail.com>
2019-05-23 00:28:35 +0200

    Add post general and project custom scripts

--- config/default_config.py
+++ config/default_config.py
@@ -4,3 +4,9 @@ controller_colors = {
     "L": {"enable_overrides": True, "use_rgb": True, "color_rgb": [0.0, 0.5, 1.0]},
     "R": {"enable_overrides": True, "use_rgb": True, "color_rgb": [1.0, 0.05, 0.05]},
 }
+
+
+# Custom Scripts
+general_scripts_dir = None
+project_scripts_dir = None
+asset_scripts_dir = {"relative": True, "path": "scripts"}
