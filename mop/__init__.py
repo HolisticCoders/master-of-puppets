368d797c9fa14d3a9eaf48dad0a94fbbaff52fa5
Loïc Pinsard <muream@gmail.com>
2019-06-19 20:03:55 +0200

    remove the vendor folder from the python path

--- mop/__init__.py
+++ mop/__init__.py
@@ -1,12 +1,4 @@
-import site
-import os
-
 import mop.internals
-
-# add vendor folder to PYTHONPATH
-curr_dir = os.path.dirname(__file__)
-vendor = os.path.abspath(os.path.join(curr_dir, "..", "vendor"))
-site.addsitedir(vendor)
 
 increment_version = mop.internals.increment_version
 incremental_save = mop.internals.incremental_save
