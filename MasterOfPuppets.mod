9682c174086d9757a0d6aca22163c7b48299facb
Muream <muream@gmail.com>
2019-07-02 09:32:24 +0200

    make mop a maya module for an cleaner installation

---
+++ MasterOfPuppets.mod
@@ -0,0 +1,2 @@
++ MasterOfPuppets 0.4 /path/to/master-of-puppets/
+scripts: /path/to/master-of-puppets/
--- userSetup.py
+++ userSetup.py
@@ -1,13 +1,9 @@
-import site
-
-site.addsitedir("/path/to/master-of-puppets/")
-
 # It is essential to set this variable to `True` for dynamic
 # reloading through the `Reload mop` menu item.
+import sys
+
 sys.dont_write_bytecode = True
 
-from maya import utils
+import maya.cmds as cmds
 
-import mop.ui.menu
-
-utils.executeDeferred(mop.ui.menu.build_menu)
+cmds.evalDeferred("import mop.ui.menu; mop.ui.menu.build_menu()")
